f88e9e459c66119d1b0be47b22f44e8f88221db7
ricard0ff <ricardo.ferreira@rackspace.co.uk>
2017-11-17 08:05:49 +0000

    initial bucket

--- src/main.go
+++ src/main.go
@@ -1,16 +1,43 @@
 package main
 
-import "cloud.google.com/go/storage"
 import (
-	"context"
 	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/net/context"
 )
 
 func main() {
-	fmt.Println("hello world")
 	ctx := context.Background()
+
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectID == "" {
+		fmt.Fprintf(os.Stderr, "GOOGLE_CLOUD_PROJECT environment variable must be set.\n")
+		os.Exit(1)
+	}
+
+	// [START setup]
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		// TODO: Handle error.
+		log.Fatal(err)
 	}
+	// [END setup]
+
+	// Give the bucket a unique name.
+	name := fmt.Sprintf("golang-example-buckets-%d", time.Now().Unix())
+	if err := create(client, projectID, name); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("created bucket: %v\n", name)
+
+	// list buckets from the project
+	buckets, err := list(client, projectID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("buckets: %+v\n", buckets)
+
 }
